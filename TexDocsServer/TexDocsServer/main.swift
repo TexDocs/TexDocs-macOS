ea38cb6fe565f8cc0ece6bf68f459346547376dd
Til Blechschmidt <til@blechschmidt.de>
2017-11-11 20:22:25 +0100

    Added initial server project

---
+++ TexDocsServer/TexDocsServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TexDocsServer
+//
+//  Created by Til Blechschmidt on 11.11.17.
+//  Copyright © 2017 Til Blechschmidt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
