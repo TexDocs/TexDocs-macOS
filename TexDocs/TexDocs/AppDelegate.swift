82dd0125eb52a065301524e1c3b962ba6c6ee6b8
Noah Peeters <noah.peeters@icloud.com>
2017-11-11 20:47:32 +0100

    Added client project

---
+++ TexDocs/TexDocs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TexDocs
+//
+//  Created by Noah Peeters on 11.11.17.
+//  Copyright © 2017 TexDocs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TexDocs/TexDocs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TexDocs
+//
+//  Created by Noah Peeters on 11.11.17.
+//  Copyright © 2017 TexDocs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
