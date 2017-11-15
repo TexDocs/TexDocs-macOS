022cb123cfd65923bceb7caf64ba4e6e1307e70e
Noah Peeters <noah.peeters@icloud.com>
2017-11-15 21:59:04 +0100

    Add documentation to document data

--- TexDocs/TexDocs/Model/Document/DocumentData.swift
+++ TexDocs/TexDocs/Model/Document/DocumentData.swift
@@ -8,31 +8,75 @@
 
 import Foundation
 
+/// Data used for saving and loading a project.
 struct DocumentData: Codable {
-    var repo: Repo? = nil
-    var server: Server? = nil
     
+    /// Collaboration data of the project.
+    var collaboration: Collaboration?
+    
+    /// Initializes the document data for a new project.
+    ///
+    /// - Parameter method: The method used to open the project.
     init(open method: NewProjectOpenMethod) {
-        if let repoURL = method.repoURL {
-            repo = Repo(url: repoURL)
+        switch method {
+        case .create(let serverURL, let repoURL):
+            collaboration = Collaboration(
+                server: Collaboration.Server(url: serverURL),
+                repo: Collaboration.Repo(url: repoURL))
+        case .join(let serverURL):
+            collaboration = Collaboration(server: Collaboration.Server(url: serverURL))
+        case .offline:
+            collaboration = nil
+        }
+    }
+    
+    /// Data required for collaboration.
+    struct Collaboration: Codable {
+        /// Collaboration server used for live collaboration.
+        var server: Server
+        
+        /// Repo used for offline collaboration.
+        var repo: Repo?
+        
+        /// Initializes collaboration data with at least a collaboration server.
+        ///
+        /// - Parameters:
+        ///   - server: Collaboration server used for live collaboration.
+        ///   - repo: Repo used for offline collaboration.
+        init(server: Server, repo: Repo? = nil) {
+            self.server = server
+            self.repo = repo
         }
         
-        if let serverURL = method.serverURL {
-            server = Server(url: serverURL)
+        struct Server: Codable {
+            /// Collaboration web socket server url.
+            let url: String
+            
+            /// Initializes collaboration server data with a url.
+            ///
+            /// - Parameter url: WebSocketServer url.
+            init(url: String) {
+                self.url = url
+            }
         }
-    }
-    
-    struct Server: Codable {
-        let url: String
-    }
-    
-    struct Repo: Codable {
-        let url: String
+        
+        struct Repo: Codable {
+            /// Git repo url
+            let url: String
+            
+            /// Initializes repo data with a url.
+            ///
+            /// - Parameter url: Git url.
+            init(url: String) {
+                self.url = url
+            }
+        }
     }
 }
 
 extension DocumentData {
-    var isOnline: Bool {
-        return server != nil
+    /// Collaboration status of the project
+    var isCollaborationEnabled: Bool {
+        return collaboration != nil
     }
 }
