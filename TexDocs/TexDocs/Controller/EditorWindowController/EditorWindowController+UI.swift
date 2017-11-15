ce124a02faf8e4d7005492d7773ec0df423df415
Noah Peeters <noah.peeters@icloud.com>
2017-11-15 22:03:17 +0100

    Refactored EditorWindowController

--- TexDocs/TexDocs/Controller/EditorWindowController/EditorWindowController+UI.swift
+++ TexDocs/TexDocs/Controller/EditorWindowController/EditorWindowController+UI.swift
@@ -1,22 +1,14 @@
 //
-//  EditorWindowController.swift
+//  EditorWindowController+UI.swift
 //  TexDocs
 //
-//  Created by Noah Peeters on 12.11.17.
+//  Created by Noah Peeters on 15.11.17.
 //  Copyright © 2017 TexDocs. All rights reserved.
 //
 
 import Cocoa
 
-class EditorWindowController: NSWindowController {
-    
-    let client = CollaborationClient(url: URL(string: "ws://localhost:8080/project/join/110ec58a-a0f2-4ac4-8393-c866d813b8d1")!)
-    
-    override func windowDidLoad() {
-        editorViewController.editorView.collaborationDelegate = self
-        client.delegate = self
-    }
-    
+extension EditorWindowController {
     var rootSplitViewController: NSSplitViewController {
         return contentViewController as! NSSplitViewController
     }
@@ -65,26 +57,5 @@ class EditorWindowController: NSWindowController {
     
     var pdfViewController: NSViewController {
         return pdfViewPanel.viewController
-    }
-    
-    @IBAction func panelsDidChange(_ sender: NSSegmentedControl) {
-        outlinePanel.isCollapsed = !sender.isSelected(forSegment: 0)
-        consolePanel.isCollapsed = !sender.isSelected(forSegment: 1)
-    }
-    
-    @IBAction func selectedModeDidChange(_ sender: NSSegmentedControl) {
-        switch sender.selectedSegment {
-        case 0:
-            centerPanel.isCollapsed = false
-            pdfViewPanel.isCollapsed = true
-        case 1:
-            centerPanel.isCollapsed = false
-            pdfViewPanel.isCollapsed = false
-        case 2:
-            centerPanel.isCollapsed = true
-            pdfViewPanel.isCollapsed = false
-        default:
-            break
-        }
     }
 }
---
+++ TexDocs/TexDocs/Controller/EditorWindowController/EditorWindowController.swift
@@ -0,0 +1,40 @@
+//
+//  EditorWindowController.swift
+//  TexDocs
+//
+//  Created by Noah Peeters on 12.11.17.
+//  Copyright © 2017 TexDocs. All rights reserved.
+//
+
+import Cocoa
+
+class EditorWindowController: NSWindowController {
+    
+    let client = CollaborationClient(url: URL(string: "ws://localhost:8080/project/join/110ec58a-a0f2-4ac4-8393-c866d813b8d1")!)
+    
+    override func windowDidLoad() {
+        editorViewController.editorView.collaborationDelegate = self
+        client.delegate = self
+    }
+    
+    @IBAction func panelsDidChange(_ sender: NSSegmentedControl) {
+        outlinePanel.isCollapsed = !sender.isSelected(forSegment: 0)
+        consolePanel.isCollapsed = !sender.isSelected(forSegment: 1)
+    }
+    
+    @IBAction func selectedModeDidChange(_ sender: NSSegmentedControl) {
+        switch sender.selectedSegment {
+        case 0:
+            centerPanel.isCollapsed = false
+            pdfViewPanel.isCollapsed = true
+        case 1:
+            centerPanel.isCollapsed = false
+            pdfViewPanel.isCollapsed = false
+        case 2:
+            centerPanel.isCollapsed = true
+            pdfViewPanel.isCollapsed = false
+        default:
+            break
+        }
+    }
+}
